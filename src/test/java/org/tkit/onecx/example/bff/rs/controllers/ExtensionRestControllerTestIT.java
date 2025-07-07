43daab9976dc81f64d377e2f628f1f99d6a42cc6
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-07-07 07:28:51 +0200

    feat: init impl

---
+++ src/test/java/org/tkit/onecx/example/bff/rs/controllers/ExtensionRestControllerTestIT.java
@@ -0,0 +1,7 @@
+package org.tkit.onecx.example.bff.rs.controllers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExtensionRestControllerTestIT extends ExtensionRestControllerTest {
+}
